cc97e196353435496477ecfeacf6c04b0d86943e
vnsawnda <146521163+vnsawnda@users.noreply.github.com>
2024-06-19 17:52:47 +0700

    app.py

--- app.py
+++ app.py
@@ -1,129 +1,150 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 import pymysql
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Membuat koneksi ke database MySQL
-conn = pymysql.connect(
-    host="kubela.id",
-    port=3306,
-    user="davis2024irwan",
-    password="wh451n9m@ch1n3",
-    database="aw"
-)
+# Sidebar select box
+select_box = st.sidebar.selectbox('Pilih data yang ingin ditampilkan:', ['Adventure Works', 'IMDb Populer Movies'])
 
-# Membuat cursor
-cursor = conn.cursor()
+# Display IMDb Popular Movies
+if select_box == 'IMDb Populer Movies':
+    # Nama file CSV
+    fn1 = 'IMDb_Populer.csv'
+    
+    # Tampilkan judul aplikasi
+    st.title("Scrapping Website IMDb")
 
-# Cek koneksi berhasil
-if conn:
-    st.sidebar.success('Connected to MySQL database')
+    # Membaca file csv ke dalam DataFrame dengan encoding 'latin1'
+    df1 = pd.read_csv(fn1, encoding='latin1')
 
-# Query untuk mengambil data dari tabel dimcustomer dan factinternetsales
-query1 = """ 
-        SELECT dc.Gender, COUNT(fs.CustomerKey) AS TotalCustomers
-        FROM dimcustomer dc
-        LEFT JOIN factinternetsales fs ON dc.CustomerKey = fs.CustomerKey
-        GROUP BY dc.Gender;
-"""
+    # Menampilkan DataFrame sebagai tabel
+    st.dataframe(df1)
 
-# Eksekusi query
-cursor.execute(query1)
+    # Display additional information or plots if needed
 
-# Mendapatkan hasil query sebagai tuple
-data1 = cursor.fetchall()
+# Display Adventure Works Data
+else:
+    # Membuat koneksi ke database MySQL
+    conn = pymysql.connect(
+        host="kubela.id",
+        port=3306,
+        user="davis2024irwan",
+        password="wh451n9m@ch1n3",
+        database="aw"
+    )
 
-# Membaca data dari database
-df_customer = pd.DataFrame(data1, columns=['Gender', 'TotalCustomers'])
+    # Membuat cursor
+    cursor = conn.cursor()
 
-# Query untuk mengambil data harga list (ListPrice) dan berat (Weight) dari tabel dimproduct
-query2 = """
-    SELECT 
-        ListPrice,
-        Weight
-    FROM 
-        dimproduct
-"""
+    # Cek koneksi berhasil
+    if conn:
+        st.sidebar.success('Connected to MySQL database')
 
-# Membaca data dari database menjadi dataframe menggunakan pandas
-df_product = pd.read_sql(query2, conn)
+    # Query untuk mengambil data dari tabel dimcustomer dan factinternetsales
+    query1 = """ 
+            SELECT dc.Gender, COUNT(fs.CustomerKey) AS TotalCustomers
+            FROM dimcustomer dc
+            LEFT JOIN factinternetsales fs ON dc.CustomerKey = fs.CustomerKey
+            GROUP BY dc.Gender;
+    """
 
-# Query untuk mengambil data warna produk dari tabel dimproduct
-query3 = """
-    SELECT 
-        Color,
-        COUNT(*) AS count
-    FROM 
-        dimproduct
-    GROUP BY 
-        Color
-"""
+    # Eksekusi query
+    cursor.execute(query1)
 
-# Membaca data dari database menjadi dataframe menggunakan pandas
-df_color = pd.read_sql(query3, conn)
+    # Mendapatkan hasil query sebagai tuple
+    data1 = cursor.fetchall()
 
-# Query untuk mengambil data total penjualan dari tabel factinternetsales
-query4 = """
-    SELECT SalesAmount
-    FROM factinternetsales
-"""
+    # Membaca data dari database
+    df_customer = pd.DataFrame(data1, columns=['Gender', 'TotalCustomers'])
 
-# Membaca data dari database menjadi dataframe menggunakan pandas
-df_sales = pd.read_sql(query4, conn)
+    # Query untuk mengambil data harga list (ListPrice) dan berat (Weight) dari tabel dimproduct
+    query2 = """
+        SELECT 
+            ListPrice,
+            Weight
+        FROM 
+            dimproduct
+    """
 
-# Menutup cursor dan koneksi database
-cursor.close()
-conn.close()
+    # Membaca data dari database menjadi dataframe menggunakan pandas
+    df_product = pd.read_sql(query2, conn)
 
-# Menampilkan judul di halaman web
-st.title("Final Project Data Visualisasi")
-st.markdown("<h1 style='text-align: center; color: black;'>Dashboard Adventure Works</h1>", unsafe_allow_html=True)
+    # Query untuk mengambil data warna produk dari tabel dimproduct
+    query3 = """
+        SELECT 
+            Color,
+            COUNT(*) AS count
+        FROM 
+            dimproduct
+        GROUP BY 
+            Color
+    """
 
-# Menampilkan data frame df_customer di Streamlit
-st.subheader("1. Total Customers Berdasarkan Gender")
-st.dataframe(df_customer)
+    # Membaca data dari database menjadi dataframe menggunakan pandas
+    df_color = pd.read_sql(query3, conn)
 
-# Membuat pie chart menggunakan Matplotlib untuk total customers berdasarkan gender
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.pie(df_customer['TotalCustomers'], labels=df_customer['Gender'], autopct='%1.1f%%', colors=['blue', 'pink'], startangle=140)
-ax.set_title('Komparasi Total Customers Berdasarkan Gender')
-st.pyplot(fig)
+    # Query untuk mengambil data total penjualan dari tabel factinternetsales
+    query4 = """
+        SELECT SalesAmount
+        FROM factinternetsales
+    """
 
-# Menampilkan data frame df_product di Streamlit
-st.subheader("2. Harga List dan Berat Produk")
-st.dataframe(df_product)
+    # Membaca data dari database menjadi dataframe menggunakan pandas
+    df_sales = pd.read_sql(query4, conn)
 
-# Membuat line plot untuk memvisualisasikan hubungan antara harga list dan berat produk
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.plot(df_product['ListPrice'], color='blue', label='Harga List')
-ax.plot(df_product['Weight'], color='green', label='Berat')
-ax.set_xlabel('Indeks Produk')
-ax.set_ylabel('Nilai')
-ax.set_title('Hubungan Antara Harga List dan Berat Produk')
-ax.legend()
-ax.grid()
-st.pyplot(fig)
+    # Menutup cursor dan koneksi database
+    cursor.close()
+    conn.close()
 
-# Menampilkan data frame df_color di Streamlit
-st.subheader("3. Komposisi Warna Produk")
-st.dataframe(df_color)
+    # Menampilkan judul di halaman web
+    st.title("Final Project Data Visualisasi")
+    st.markdown("<h1 style='text-align: center; color: black;'>Dashboard Adventure Works</h1>", unsafe_allow_html=True)
 
-# Membuat pie chart menggunakan Matplotlib untuk komposisi warna produk
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.pie(df_color['count'], labels=df_color['Color'], autopct='%1.1f%%', startangle=140)
-ax.set_title('Komposisi Warna Produk')
-ax.axis('equal')  # Memastikan lingkaran berbentuk lingkaran
-st.pyplot(fig)
+    # Menampilkan data frame df_customer di Streamlit
+    st.subheader("1. Total Customers Berdasarkan Gender")
+    st.dataframe(df_customer)
 
-# Menampilkan data frame df_sales di Streamlit
-st.subheader("4. Distribusi Jumlah Penjualan")
-st.dataframe(df_sales)
+    # Membuat pie chart menggunakan Matplotlib untuk total customers berdasarkan gender
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.pie(df_customer['TotalCustomers'], labels=df_customer['Gender'], autopct='%1.1f%%', colors=['blue', 'pink'], startangle=140)
+    ax.set_title('Komparasi Total Customers Berdasarkan Gender')
+    st.pyplot(fig)
 
-# Membuat histogram untuk visualisasi distribusi jumlah penjualan
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.histplot(df_sales['SalesAmount'], kde=False, color='skyblue', bins=30, ax=ax)
-ax.set_xlabel('Sales Amount')
-ax.set_ylabel('Frequency')
-ax.set_title('Distribusi Jumlah Penjualan')
-st.pyplot(fig)
+    # Menampilkan data frame df_product di Streamlit
+    st.subheader("2. Harga List dan Berat Produk")
+    st.dataframe(df_product)
+
+    # Membuat line plot untuk memvisualisasikan hubungan antara harga list dan berat produk
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(df_product['ListPrice'], color='blue', label='Harga List')
+    ax.plot(df_product['Weight'], color='green', label='Berat')
+    ax.set_xlabel('Indeks Produk')
+    ax.set_ylabel('Nilai')
+    ax.set_title('Hubungan Antara Harga List dan Berat Produk')
+    ax.legend()
+    ax.grid()
+    st.pyplot(fig)
+
+    # Menampilkan data frame df_color di Streamlit
+    st.subheader("3. Komposisi Warna Produk")
+    st.dataframe(df_color)
+
+    # Membuat pie chart menggunakan Matplotlib untuk komposisi warna produk
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.pie(df_color['count'], labels=df_color['Color'], autopct='%1.1f%%', startangle=140)
+    ax.set_title('Komposisi Warna Produk')
+    ax.axis('equal')  # Memastikan lingkaran berbentuk lingkaran
+    st.pyplot(fig)
+
+    # Menampilkan data frame df_sales di Streamlit
+    st.subheader("4. Distribusi Jumlah Penjualan")
+    st.dataframe(df_sales)
+
+    # Membuat histogram untuk visualisasi distribusi jumlah penjualan
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.histplot(df_sales['SalesAmount'], kde=False, color='skyblue', bins=30, ax=ax)
+    ax.set_xlabel('Sales Amount')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Distribusi Jumlah Penjualan')
+    st.pyplot(fig)
