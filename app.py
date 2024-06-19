d78712ad0eb6e8ffa9e7db9e06aeb87000006c3f
vnsawnda <146521163+vnsawnda@users.noreply.github.com>
2024-06-19 17:33:32 +0700

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if conn:
     st.sidebar.success('Connected to MySQL database')
 
 # Query untuk mengambil data dari tabel dimcustomer dan factinternetsales
-query = """ 
+query1 = """ 
         SELECT dc.Gender, COUNT(fs.CustomerKey) AS TotalCustomers
         FROM dimcustomer dc
         LEFT JOIN factinternetsales fs ON dc.CustomerKey = fs.CustomerKey
@@ -28,27 +28,16 @@ query = """
 """
 
 # Eksekusi query
-cursor.execute(query)
+cursor.execute(query1)
 
 # Mendapatkan hasil query sebagai tuple
-data = cursor.fetchall()
-
-# Menutup cursor dan koneksi database
-cursor.close()
-conn.close()
+data1 = cursor.fetchall()
 
 # Membaca data dari database
-df_customer = pd.DataFrame(data, columns=['Gender', 'TotalCustomers'])
-
-# Menampilkan judul di halaman web
-st.title("Final Project Data Visualisasi")
-st.markdown("<h1 style='text-align: center; color: black;'>Dashboard Adventure Works</h1>", unsafe_allow_html=True)
-
-# Menampilkan data frame di Streamlit
-st.dataframe(df_customer)
+df_customer = pd.DataFrame(data1, columns=['Gender', 'TotalCustomers'])
 
 # Query untuk mengambil data harga list (ListPrice) dan berat (Weight) dari tabel dimproduct
-query = """
+query2 = """
     SELECT 
         ListPrice,
         Weight
@@ -57,21 +46,29 @@ query = """
 """
 
 # Membaca data dari database menjadi dataframe menggunakan pandas
-df = pd.read_sql(query, connection)
+df_product = pd.read_sql(query2, conn)
 
-# Plotting menggunakan line plot untuk memvisualisasikan hubungan antara harga list dan berat produk
+# Menutup cursor dan koneksi database
+cursor.close()
+conn.close()
+
+# Menampilkan judul di halaman web
+st.title("Final Project Data Visualisasi")
+st.markdown("<h1 style='text-align: center; color: black;'>Dashboard Adventure Works</h1>", unsafe_allow_html=True)
+
+# Menampilkan data frame di Streamlit
+st.dataframe(df_customer)
+
+# Membuat line plot untuk memvisualisasikan hubungan antara harga list dan berat produk
 plt.figure(figsize=(10, 6))
-plt.plot(df['ListPrice'], color='blue', label='Harga List')
-plt.plot(df['Weight'], color='green', label='Berat')
+plt.plot(df_product['ListPrice'], color='blue', label='Harga List')
+plt.plot(df_product['Weight'], color='green', label='Berat')
 plt.xlabel('Indeks Produk')
 plt.ylabel('Nilai')
 plt.title('Hubungan Antara Harga List dan Berat Produk')
 plt.legend()
 plt.grid()
-
-# Menampilkan plot
-plt.tight_layout()
-plt.show()
+st.pyplot(plt)
 
 # Membuat pie chart menggunakan Matplotlib
 fig, ax = plt.subplots(figsize=(8, 8))
