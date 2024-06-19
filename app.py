6047428221bd792d19f711f3dfaf43d25c1d0be6
vnsawnda <146521163+vnsawnda@users.noreply.github.com>
2024-06-19 17:29:18 +0700

    app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ cursor = conn.cursor()
 
 # Cek koneksi berhasil
 if conn:
-    print('Connected to MySQL database')
+    st.sidebar.success('Connected to MySQL database')
 
 # Query untuk mengambil data dari tabel dimcustomer dan factinternetsales
 query = """ 
@@ -38,20 +38,17 @@ cursor.close()
 conn.close()
 
 # Membaca data dari database
-df_customer = pd.DataFrame(data_cust, columns=['Gender', 'TotalCustomers'])
-
-return df_customer
+df_customer = pd.DataFrame(data, columns=['Gender', 'TotalCustomers'])
 
 # Menampilkan judul di halaman web
 st.title("Dashboard Adventure Works")
-
 st.markdown("<h1 style='text-align: center; color: black;'>Dashboard Adventure Works</h1>", unsafe_allow_html=True)
 
+# Menampilkan data frame di Streamlit
 st.dataframe(df_customer)
-plt.figure(figsize=(8, 8))
-plt.pie(df_customer['Gender'], df_customer['TotalCustomers'], autopct='%1.1f%%', colors=['blue', 'pink'], startangle=140)
-plt.title('Komparasi Total Customers Berdasarkan Gender')
-plt.xlabel('Gender')
-plt.ylabel('Total Customer')
-plt.grid(True)
-st.pyplot(plt)
+
+# Membuat pie chart menggunakan Matplotlib
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.pie(df_customer['TotalCustomers'], labels=df_customer['Gender'], autopct='%1.1f%%', colors=['blue', 'pink'], startangle=140)
+ax.set_title('Komparasi Total Customers Berdasarkan Gender')
+st.pyplot(fig)
