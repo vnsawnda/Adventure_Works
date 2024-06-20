3e5677bf1b59c5e3766a56c5afa40572d3eecc5c
vnsawnda <146521163+vnsawnda@users.noreply.github.com>
2024-06-20 21:55:47 +0700

    app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ else:
 
     # Menampilkan data frame df_customer di Streamlit
     st.subheader("1. Komparasi Total Customers Berdasarkan Gender")
-    ax.set_title("Visualisasi ini digunakan untuk membandingkan jumlah pelanggan berdasarkan gender, sehingga memudahkan kita untuk mengetahui berapa banyak pelanggan wanita dan berapa banyak pelanggan pria.")
+    st.title("Visualisasi ini digunakan untuk membandingkan jumlah pelanggan berdasarkan gender, sehingga memudahkan kita untuk mengetahui berapa banyak pelanggan wanita dan berapa banyak pelanggan pria.")
     st.dataframe(df_customer)
 
     # Membuat pie chart menggunakan Matplotlib untuk total customers berdasarkan gender
